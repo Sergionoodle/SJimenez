6c27e26f38f2f438c0be2cec99028559f7ebcd73
sergio <asergiolemolaelazucar@gmail.com>
2021-09-29 18:59:28 +0200

    Initial commit

---
+++ pruena.php
@@ -0,0 +1,16 @@
+<?php
+echo "Gracias Dani :)";
+
+$nombre = "Sergio";
+
+echo "<br>Mi nombre es $nombre";
+
+echo "<br>";
+
+$numeroUno = 5;
+$numeroDos = 6;
+
+if ($numeroUno < $numeroDos){
+    echo "<br> El numero mayor es el numero $numeroDos";
+}
+?>
