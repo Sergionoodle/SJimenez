d2654a0d8b9a413d9a678e03f4f3c124f4bf23de
sergio <asergiolemolaelazucar@gmail.com>
2021-10-04 16:09:48 +0200

    actualizado

--- SJimenez/find_n_perfect.php
+++ SJimenez/find_n_perfect.php
@@ -14,15 +14,33 @@
     <?php
     function getDivisors($num){
         //TODO: YOUR CODE HERE
+        for($i = 1; $i <= $num; $i++ ){
+            if($num % $i == 0) {
+
+                $divisores[] = $i;
+
+            }
+        }
+        return $divisores;
     }
 
     function isPerfectNum($num){
         //TODO: YOUR CODE HERE
+       $x = 1;
+       for($i = 1; $x <= $num; $i++){
+
+           $div = getDivisors($i);
+           $cont = $div;
+           if(count(cont)==2 ){
+
+           }
+       }
     }
 
     if (isset($_POST["num"])) {
         $num = intval($_POST["num"]);
         //TODO: YOUR CODE HERE
+        isPrimeNum();
     }
     ?>
 </div>
