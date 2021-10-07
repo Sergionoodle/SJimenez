6013741aafb52419e648c165618957035b1ce1b4
sergio <asergiolemolaelazucar@gmail.com>
2021-10-07 16:51:00 +0200

    ejercicio terminado

--- SJimenez/chrismas_tree.php
+++ SJimenez/chrismas_tree.php
@@ -13,18 +13,24 @@
 
 <div>
     <?php
-    $num = $_POST["num"]; //Con esto hacemos que le de el valor de num
+    $num = $_POST["num"];
 
-    for($i = 0; $i <= $num; $i++){
-        //Con este vamos a hacerlo de izquierda a derecha y cuando acabe
+    for ($i = 0; $i <= $num; $i++){
 
-        for($x = 1; $x <= $i; $x++){
-
-            echo "*";
+        for($y = 0; $y <= $num - $i; $y++){
+            echo "&nbsp ";//en este bucle le damos los primeros espacios
         }
-        echo "<br>";
 
-    }
+        for ($x = 0; $x < $i *2 - 1 ; $x++){
+            echo "*";//este es el bucle que usamos para ir añadiendo *, donde la i la multiplicamos por dos y le quitamos uno para que de la punta y vaya equitativamente
+
+        }
+        for($z = 0; $z <= $num + $i;$z++){
+            echo "&nbsp";
+        }//este es para dar espacios por la derecha, realmente no es necesario
+
+        echo "<br>";
+    }//con el primer bucle lo que hacemos es ir dandole a cada * un salto de linea
 
     ?>
 </div>
