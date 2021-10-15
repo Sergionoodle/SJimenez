ed587e6b3369597b74028710ffefc95e1c0e380b
sergio <asergiolemolaelazucar@gmail.com>
2021-10-15 17:28:01 +0200

    ejercicio actualizado 2 a medio hacer

--- SJimenez/arrays/elefantes.php
+++ SJimenez/arrays/elefantes.php
@@ -3,14 +3,17 @@ $contents = file_get_contents("https://dawsonferrer.com/allabres/apis_solutions/
 $elephants = json_decode($contents, true);
 
 function getSortedElephantsByNumber($elephants){
+
+
     //TODO: Return an array of elephants sorted by it's number (ascending order).
     //NOTES 1: You receive a elephants multidimensional array, you can view it's content with var_dump() function.
     //NOTES 2:You CAN'T use any sorting PHP built-in function.
+
     for($fila = 0; $fila < count($elephants); $fila++){
 //recorremos las filas de la array
         for($columna = 0; $columna < count($elephants); $columna++){
 //recorremos las columnas de la array
-            if($elephants[$columna]['Number']> $elephants[$columna+1]['Number']&&$elephants[$columna+1]['Number'] != null){
+            if($elephants[$columna]['number']> $elephants[$columna+1]['number']&&$elephants[$columna+1]['number'] != null){
                 //Comparamos que la columna del numero el resultado sea mayor al resultado mas uno y que sea diferente de null
 
                 $n1 = $elephants[$columna];
@@ -73,9 +76,18 @@ function getSortedElephantsByNumber($elephants){
     <?php
     //TODO: Logic to print the table body.
 
+    $orden = getSortedElephantsByNumber($elephants);
 
-    var_dump($elephants);
-
+    for ($i = 0; $i < count($elephants); $i++) {
+        echo "<tr>";
+        echo "<td>".$elephants[$i]['number']."</td>";
+        echo "<td>".$elephants[$i]['name']."</td>";
+        echo "<td>".$elephants[$i]['species']."</td>";
+        echo "<td>".orden[$i]['number']."</td>";
+        echo "<td>".$orden[$i]['number']."</td>";
+        echo "<td>".$orden[$i]['number']."</td>";
+        echo "</tr>";
+    }
     ?>
     </tbody>
 </table>
