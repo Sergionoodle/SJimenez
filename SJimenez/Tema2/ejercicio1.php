f50346f246b9bbb4f5f578321899b014ca8dcec7
sergio <asergiolemolaelazucar@gmail.com>
2021-10-25 15:51:39 +0200

    a corregir

--- SJimenez/Tema2/ejercicio1.php
+++ SJimenez/Tema2/ejercicio1.php
@@ -1,243 +1,47 @@
 <?php
-$seed = 0000; //TODO: LAST 4 NUMBERS OF YOUR DNI.
+
+include ("Character.php");
+include ("Episodes.php");
+include ("Locations.php");
+
+$seed = 4312; //TODO: LAST 4 NUMBERS OF YOUR DNI.
 $api_url = "https://dawsonferrer.com/allabres/apis_solutions/rickandmorty/api.php?seed=" . $seed . "&data=";
 
-$characters = json_decode(file_get_contents($api_url . "characters"), true);
-$episodes = json_decode(file_get_contents($api_url . "episodes"), true);
-$locations = json_decode(file_get_contents($api_url . "locations"), true);
+$charactersJ = json_decode(file_get_contents($api_url . "characters"), true);
+$episodesJ = json_decode(file_get_contents($api_url . "episodes"), true);
+$locationsJ = json_decode(file_get_contents($api_url . "locations"), true);
 
-class Character{
+var_dump($locationsJ);
 
-    private $id;
-    private $name;
-    private $status;
-    private $especies;
-    private $type;
-    private $gender;
-    private $origin;
-    private $location;
-    private $image;
-    private $created;
-    private $episodes;
+function obCharacters($charactersJ)
+{
 
-    public function __construct()
-    {
+    for ($i = 0; $i < count($charactersJ); $i++) {
 
-    }
-
-
-    /**
-     * @return mixed
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * @param mixed $id
-     */
-    public function setId($id)
-    {
-        $this->id = $id;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    /**
-     * @param mixed $name
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getStatus()
-    {
-        return $this->status;
-    }
-
-    /**
-     * @param mixed $status
-     */
-    public function setStatus($status)
-    {
-        $this->status = $status;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getEspecies()
-    {
-        return $this->especies;
-    }
-
-    /**
-     * @param mixed $especies
-     */
-    public function setEspecies($especies)
-    {
-        $this->especies = $especies;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getType()
-    {
-        return $this->type;
-    }
-
-    /**
-     * @param mixed $type
-     */
-    public function setType($type)
-    {
-        $this->type = $type;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getGender()
-    {
-        return $this->gender;
-    }
-
-    /**
-     * @param mixed $gender
-     */
-    public function setGender($gender)
-    {
-        $this->gender = $gender;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getOrigin()
-    {
-        return $this->origin;
-    }
-
-    /**
-     * @param mixed $origin
-     */
-    public function setOrigin($origin)
-    {
-        $this->origin = $origin;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getLocation()
-    {
-        return $this->location;
-    }
-
-    /**
-     * @param mixed $location
-     */
-    public function setLocation($location)
-    {
-        $this->location = $location;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getImage()
-    {
-        return $this->image;
-    }
-
-    /**
-     * @param mixed $image
-     */
-    public function setImage($image)
-    {
-        $this->image = $image;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getCreated()
-    {
-        return $this->created;
-    }
-
-    /**
-     * @param mixed $created
-     */
-    public function setCreated($created)
-    {
-        $this->created = $created;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getEpisodes()
-    {
-        return $this->episodes;
-    }
-
-    /**
-     * @param mixed $episodes
-     */
-    public function setEpisodes($episodes)
-    {
-        $this->episodes = $episodes;
-    }
-
-}
-
-for ($i = 0; $i < count($characters);$i++) {
-    for ($j = 0; $j < count($characters); $j++) {
-
-        if ($characters['id'][$i] < $characters['id'][$j]) {
-
-            $aux = $characters[$i];
-            $characters[$i] = $characters[$j];
-            $characters[$j] = $aux;
-
-
-        }
-
+        $characters[$i] = new Character($charactersJ[$i]['id'], $charactersJ[$i]['name'],
+            $charactersJ[$i]['status'], $charactersJ[$i]['species'], $charactersJ[$i]['type'],
+            $charactersJ[$i]['gender'], $charactersJ[$i]['origin'], $charactersJ[$i]['location'],
+            $charactersJ[$i]['image'], $charactersJ[$i]['created'], $charactersJ[$i]['episodes']);
     }
     return $characters;
 }
-class Episodes{
 
-    private $id;
-    private $name;
-    private  $air_date;
-    private $episode;
-    private $create;
-    private $character;
+function odEpisodes($episodesJ){
 
+    for ($i = 0; $i < count($episodesJ); $i++){
+
+        $episodes[$i] = new Episodes($episodesJ[$i]['id'], $episodesJ[$i]['name'], $episodesJ[$i]['air_date'],
+        $episodesJ[$i]['episode'], $episodesJ[$i]['created'], $episodesJ[$i]['characters']);
+    }
+return $episodes;
 }
 
-class Locations{
+function obLocations($locationsJ){
 
-    private $id;
-    private $name;
-    private $type;
-    private $dimensions;
-    private $created;
-    private $residents;
-
+    for ($i = 0; $i < count( $locationsJ); $i++){
+        $locations[$i] = new Locations($locationsJ[$i]['id'], $locationsJ[$i]['name'], $locationsJ[$i]['type'],
+            $locationsJ[$i]['dimension'], $locationsJ[$i]['created'], $locationsJ[$i]['residents']);
+    }
+    return $locations;
 }
 ?>
