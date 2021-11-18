a4a6db62c6c844f2ba22773fb9aba9d7c1f4ac3c
sergio <asergiolemolaelazucar@gmail.com>
2021-11-18 17:07:18 +0100

    ejercicio comentado y perfecto

--- SJimenez/PhpSQL/rickymorty/sqlrickymorty.php
+++ SJimenez/PhpSQL/rickymorty/sqlrickymorty.php
@@ -1,75 +1,97 @@
 <?php
 
+//API DE DATOS
 $api_url = "https://dawsonferrer.com/allabres/apis_solutions/rickandmorty/api.php?seed=" . 4444 . "&data=";
-
 $charactersJ = json_decode(file_get_contents($api_url . "characters"), true);
 $episodesJ = json_decode(file_get_contents($api_url . "episodes"), true);
 $locationsJ = json_decode(file_get_contents($api_url . "locations"), true);
 
+//CREDENCIALES
 $servername ="localhost";
 $username = "root";
 $password = "";
 $dbname ="rickymorty";
 
+//CONEXION
 $conn = new mysqli($servername, $username, $password,$dbname);
-
 if($conn->connect_error){
     echo "No se pudo conectar". $conn->connect_error;
 }else{
     echo  "Connected successfully";
 }
 
-/* --CREAMOS LA BASE DE DATOS-- *
-$sql = "CREATE DATABASE rickymorty";
-
+//  --CREAMOS LA BASE DE DATOS--
+$sql = "CREATE DATABASE IF NOT EXISTS rickymorty";
 if($conn->query($sql) === TRUE ){
-    echo "BASE DE DATOS CREADA";
+    echo "<br>BASE DE DATOS CREADA";
 }else{
     echo "ERROR ".$conn->error;
 }
-*/
 
-$sql="";
+// --CREAMOS LAS TABLAS--
+    $sql ="";
+    $sql .= "CREATE TABLE IF NOT EXISTS characters(";
+    $sql .= "id int PRIMARY KEY,";
+    $sql .= "name varchar(255),";
+    $sql .= "status varchar(255),";
+    $sql .= "specie varchar(255),";
+    $sql.= "type varchar(255)";
+    $sql.=",gender varchar(255),";
+    $sql.="origin varchar(255),";
+    $sql .= "location varchar(255),";
+    $sql.="image varchar(255),";
+    $sql .= "created varchar(255));";
 
-for ($i = 0; $i < count($charactersJ); $i++) {
-
-    $charid[]=$charactersJ[$i]["id"];
-    $charid[$i]=$conn->real_escape_string($charid[$i]);
-
-    $charname[]=$charactersJ[$i]["name"];
-    $charname[$i]=$conn->real_escape_string($charname[$i]);
-
-    $charstat[] = $charactersJ[$i]['status'];
-    $charstat[$i]=$conn->real_escape_string($charstat[$i]);
-
-    $charspec[] = $charactersJ[$i]['species'];
-    $charspec[$i]=$conn->real_escape_string($charspec[$i]);
-
-    $typech[] = $charactersJ[$i]['type'];
-    $typech[$i]=$conn->real_escape_string($typech[$i]);
-
-    $gen[] = $charactersJ[$i]['gender'];
-    $gen[$i]=$conn->real_escape_string($gen[$i]);
-
-    $orin[] = $charactersJ[$i]['origin'];
-    $orin[$i]=$conn->real_escape_string($orin[$i]);
-    $loca[] = $charactersJ[$i]['location'];
-    $loca[$i]=$conn->real_escape_string($loca[$i]);
-    $img[] = $charactersJ[$i]['image'];
-    $img[$i]=$conn->real_escape_string($img[$i]);
-    $creat[] = $charactersJ[$i]['created'];
-    $creat[$i]=$conn->real_escape_string($creat[$i]);
-
-    $sql .= "INSERT INTO characters (id_characters, name_characters, status, specie, type, gender, origin, location, image, created) VALUES (";
-    $sql .= "" . $charid[$i] . ",'" . $charname[$i] . "','" . $charstat[$i] . "','" .  $charspec[$i] . "','" . $typech[$i] . "','" . $gen[$i] . "'," . $orin[$i] . "," .$loca[$i] . ",'" . $img[$i] . "','" . $creat[$i]."'";
-    $sql .= ");";
-}
-if ($conn->multi_query($sql) === TRUE){
-    echo "Se ha creado correctamente";
+if($conn->multi_query($sql) === TRUE){
+    echo "<br>CREADA CON EXITO";
 }else {
-    echo $conn->error;
-
+    echo "ERROR" . $conn->error;
 }
+
+    $sql = "";
+    $sql .= "CREATE TABLE IF NOT EXISTS episodes";
+    $sql .= "(id int PRIMARY KEY, name varchar(255),";
+    $sql .= "air_date varchar(255),";
+    $sql .= "episode varchar(255),";
+    $sql .= "created varchar(255));";
+
+if($conn->multi_query($sql) === TRUE){
+    echo "<br>CREADA CON EXITO";
+}else {
+    echo "ERROR" . $conn->error;
+}
+    //Creamos esta tabla porque una de las tablas de la api tiene una array
+    $sql = "";
+    $sql .= "CREATE TABLE IF NOT EXISTS episodesAndCharacters (";
+    $sql .= "epiCharId int AUTO_INCREMENT PRIMARY KEY,";
+    $sql .= "idCharacter int,";
+    $sql .= "idEpisodes int,";
+    $sql .= "FOREIGN KEY (idCharacter) REFERENCES characters(id) ON DELETE CASCADE,";
+    $sql .= "FOREIGN KEY (idEpisodes) REFERENCES episodes(id) ON DELETE CASCADE);";
+if($conn->multi_query($sql) === TRUE){
+    echo "<br>CREADA CON EXITO";
+}else{
+    echo "ERROR".$conn->error;
+}
+
+    $sql ="";
+    $sql .= "CREATE TABLE IF NOT EXISTS location (";
+    $sql .= "id int PRIMARY KEY,";
+    $sql .= "name varchar(255),";
+    $sql .= "type varchar(255),";
+    $sql .= "dimension varchar(255),";
+    $sql .= "created varchar(255));";
+if($conn->multi_query($sql) === TRUE){
+    echo "<br>CREADA CON EXITO";
+}else{
+    echo "ERROR".$conn->error;
+}
+
+
+
+
+
+
 $conn->close();
 
 ?>
